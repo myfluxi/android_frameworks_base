b2e181dea7dd343d7cec663aa64c174e5c12ab74
Christian Troy <justcavalli@gmail.com>
2012-06-07 18:56:37 +0200

    fixed status bar volume control

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -1400,9 +1400,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	    loadStringSetting(stmt, Settings.System.HDMI_RESOLUTION,
                     R.string.def_hdmi_resolution);
-
+            
             loadIntegerSetting(stmt, Settings.System.VOLUME_SYSBAR,
-                    R.integer.def_volume_sysbar);
+            	    R.integer.def_volume_sysbar);
 
         } finally {
             if (stmt != null) stmt.close();
--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -52,7 +52,6 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.storage.StorageManager;
 import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
 import android.text.TextUtils;
 import android.util.Pair;
 import android.util.Slog;
@@ -1096,6 +1095,9 @@ public class TabletStatusBar extends StatusBar implements
     }
 
     private void setNavigationVisibility(int visibility) {
+    	ContentResolver resolver = mContext.getContentResolver();
+    	boolean mShowVolume = (Settings.System.getInt(resolver,
+    		Settings.System.VOLUME_SYSBAR, 1) == 1);
         boolean disableHome = ((visibility & StatusBarManager.DISABLE_HOME) != 0);
         boolean disableRecent = ((visibility & StatusBarManager.DISABLE_RECENT) != 0);
         boolean disableBack = ((visibility & StatusBarManager.DISABLE_BACK) != 0);
@@ -1105,14 +1107,13 @@ public class TabletStatusBar extends StatusBar implements
         mBackButton.setVisibility(disableBack ? View.INVISIBLE : View.VISIBLE);
         mHomeButton.setVisibility(disableHome ? View.INVISIBLE : View.VISIBLE);
         mRecentButton.setVisibility(disableRecent ? View.INVISIBLE : View.VISIBLE);
-
-        try {
-        	if (Settings.System.getInt(mContext.getContentResolver(), Settings.System.VOLUME_SYSBAR) == 1) {
-        		mVolumeUpButton.setVisibility(disableVolumeUp ? View.INVISIBLE : View.VISIBLE);
-        		mVolumeDownButton.setVisibility(disableVolumeDown ? View.INVISIBLE : View.VISIBLE);
-        	}
-        } catch (SettingNotFoundException snfe) {
-        	Slog.d(TAG, Settings.System.VOLUME_SYSBAR + " setting does not exist");
+        
+        if (mShowVolume) {
+        	mVolumeUpButton.setVisibility(disableVolumeUp ? View.INVISIBLE : View.VISIBLE);
+        	mVolumeDownButton.setVisibility(disableVolumeDown ? View.INVISIBLE : View.VISIBLE);
+        } else {
+        	mVolumeUpButton.setVisibility(View.INVISIBLE);
+        	mVolumeDownButton.setVisibility(View.INVISIBLE);
         }
 
         mInputMethodSwitchButton.setScreenLocked(
