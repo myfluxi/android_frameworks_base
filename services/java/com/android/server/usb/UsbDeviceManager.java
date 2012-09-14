99f4ce94b95d47969c4a45c50cdcd606b3b2dcf3
Alan Marchesan <justcavalli@gmail.com>
2012-09-14 06:58:46 +0200

    Add a wake lock when connected to USB

--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -40,6 +40,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Parcelable;
 import android.os.ParcelFileDescriptor;
+import android.os.PowerManager;
 import android.os.Process;
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
@@ -115,6 +116,9 @@ public class UsbDeviceManager {
     private Map<String, List<Pair<String, String>>> mOemModeMap;
     private String[] mAccessoryStrings;
 
+    private PowerManager.WakeLock wl;
+    private int wlref = 0;
+
     private class AdbSettingsObserver extends ContentObserver {
         public AdbSettingsObserver() {
             super(null);
@@ -163,6 +167,9 @@ public class UsbDeviceManager {
         initRndisAddress();
 
         readOemUsbOverrideConfig();
+
+	PowerManager power = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+	wl = power.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
 
         // create a thread for our Handler
         HandlerThread thread = new HandlerThread("UsbDeviceManager",
@@ -225,6 +232,23 @@ public class UsbDeviceManager {
 
         if (functions != null) {
             setCurrentFunctions(functions, false);
+        }
+    }
+
+    /* In usb device connected to pc host, we should create a partial wakelock to prevent go to standby*/
+    private void enableWakeLock(boolean enable){
+        if(enable){
+            Slog.d(TAG, "enable "+ TAG +" wakelock"+" wlref = "+ wlref);            
+            if(wlref==0){
+                wlref++;
+                wl.acquire();
+            }            
+        }else{
+            Slog.d(TAG, "disable "+ TAG +" wakelock"+" wlref = "+ wlref);              
+            if(wlref==1){
+                wl.release();
+                wlref--;
+            }
         }
     }
 
@@ -591,6 +615,7 @@ public class UsbDeviceManager {
                 case MSG_UPDATE_STATE:
                     mConnected = (msg.arg1 == 1);
                     mConfigured = (msg.arg2 == 1);
+		    enableWakeLock(mConnected);
                     updateUsbNotification();
                     updateAdbNotification();
                     if (containsFunction(mCurrentFunctions,
