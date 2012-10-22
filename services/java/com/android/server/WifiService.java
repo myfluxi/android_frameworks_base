162fed08e4ccdc6c90c79f6fbbe1a9e6d8d8e9f9
myfluxi <mvdw@arcor.de>
2012-10-23 00:19:38 +0200

    Properly aquire wifi wakelock
    Change-Id: I7ba3389c0fe5a3da263b3af889e1e79a6b247d99

--- services/java/com/android/server/WifiService.java
+++ services/java/com/android/server/WifiService.java
@@ -972,7 +972,7 @@ public class WifiService extends IWifiManager.Stub {
                     Slog.d(TAG, "ACTION_SCREEN_ON");
                 }
                 mAlarmManager.cancel(mIdleIntent);
-		mWifiStateMachine.releaseKeepaliveLock();
+                mWifiStateMachine.releaseKeepaliveLock();
                 mScreenOff = false;
                 evaluateTrafficStatsPolling();
                 setDeviceIdleAndUpdateWifi(false);
@@ -989,18 +989,16 @@ public class WifiService extends IWifiManager.Stub {
                  * or plugged in to AC).
                  */
                 if (!shouldWifiStayAwake(stayAwakeConditions, mPluggedType)) {
-		  mWifiStateMachine.acquireKeepaliveLock();
                     //Delayed shutdown if wifi is connected
-                    //if (mNetworkInfo.getDetailedState() == DetailedState.CONNECTED) {
-		    if (true) {
+                    if (mNetworkInfo.getDetailedState() == DetailedState.CONNECTED) {
                         if (DBG) Slog.d(TAG, "setting ACTION_DEVICE_IDLE: " + idleMillis + " ms");
                         mAlarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
                                 + idleMillis, mIdleIntent);
                     } else {
                         setDeviceIdleAndUpdateWifi(true);
                     }
-		} else {
-		    mWifiStateMachine.acquireKeepaliveLock();
+                } else {
+                    mWifiStateMachine.acquireKeepaliveLock();
                 }
             } else if (action.equals(ACTION_DEVICE_IDLE)) {
                 setDeviceIdleAndUpdateWifi(true);
